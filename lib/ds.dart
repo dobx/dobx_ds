c45c844f9c9a74d2a21b5e34696a02a85a7de70e
dyu <david.yu.ftw@gmail.com>
2017-02-16 12:05:26 +0800

    gsd

--- lib/ds.dart
+++ lib/ds.dart
@@ -32,13 +32,6 @@ class ParamId {
     return new ParamId()
       .._id = id;
   }
-  static ParamId $createObservable({
-    int id = 0,
-  }) {
-    return new _ParamId()
-      .._id = id;
-  }
-
   static const int FN_ID = 1;
 
   int _id;
@@ -47,11 +40,6 @@ class ParamId {
   int get id => _id;
   void set id(int id) { _id = id; }
 }
-class _ParamId extends ParamId with PubSub {
-  int get id { $sub(1); return _id; }
-  void set id(int id) { if (id != null && id == _id) return; _id = id ?? 0; $pub(1); }
-}
-
 
 
 
@@ -87,15 +75,6 @@ class ParamKey {
       .._key = key
       .._parentKey = parentKey;
   }
-  static ParamKey $createObservable({
-    String key = "",
-    String parentKey = "",
-  }) {
-    return new _ParamKey()
-      .._key = key
-      .._parentKey = parentKey;
-  }
-
   static const int FN_KEY = 1;
   static const int FN_PARENT_KEY = 2;
 
@@ -110,14 +89,6 @@ class ParamKey {
   String get parentKey => _parentKey;
   void set parentKey(String parentKey) { _parentKey = parentKey; }
 }
-class _ParamKey extends ParamKey with PubSub {
-  String get key { $sub(1); return _key; }
-  void set key(String key) { if (key != null && key == _key) return; _key = key ?? ""; $pub(1); }
-
-  String get parentKey { $sub(2); return _parentKey; }
-  void set parentKey(String parentKey) { if (parentKey != null && parentKey == _parentKey) return; _parentKey = parentKey ?? ""; $pub(2); }
-}
-
 
 
 
@@ -158,17 +129,6 @@ class ParamUpdate {
       .._mc = mc
       .._id = id;
   }
-  static ParamUpdate $createObservable({
-    String key = "",
-    MultiCAS mc,
-    int id = 0,
-  }) {
-    return new _ParamUpdate()
-      .._key = key
-      .._mc = mc ?? MultiCAS.$createObservable()
-      .._id = id;
-  }
-
   static const int FN_KEY = 1;
   static const int FN_MC = 2;
   static const int FN_ID = 3;
@@ -189,17 +149,6 @@ class ParamUpdate {
   int get id => _id;
   void set id(int id) { _id = id; }
 }
-class _ParamUpdate extends ParamUpdate with PubSub {
-  String get key { $sub(1); return _key; }
-  void set key(String key) { if (key != null && key == _key) return; _key = key ?? ""; $pub(1); }
-
-  MultiCAS get mc { $sub(2); return _mc; }
-  void set mc(MultiCAS mc) { if (mc != null && mc == _mc) return; _mc = mc; $pub(2); }
-
-  int get id { $sub(3); return _id; }
-  void set id(int id) { if (id != null && id == _id) return; _id = id ?? 0; $pub(3); }
-}
-
 
 
 
@@ -245,19 +194,6 @@ class ParamRangeKey {
       .._startKey = startKey
       .._parentKey = parentKey;
   }
-  static ParamRangeKey $createObservable({
-    bool desc = false,
-    int limit = 10,
-    String startKey = "",
-    String parentKey = "",
-  }) {
-    return new _ParamRangeKey()
-      .._desc = desc
-      .._limit = limit
-      .._startKey = startKey
-      .._parentKey = parentKey;
-  }
-
   static const int FN_DESC = 1;
   static const int FN_LIMIT = 2;
   static const int FN_START_KEY = 3;
@@ -284,20 +220,6 @@ class ParamRangeKey {
   String get parentKey => _parentKey;
   void set parentKey(String parentKey) { _parentKey = parentKey; }
 }
-class _ParamRangeKey extends ParamRangeKey with PubSub {
-  bool get desc { $sub(1); return _desc; }
-  void set desc(bool desc) { if (desc != null && desc == _desc) return; _desc = desc ?? false; $pub(1); }
-
-  int get limit { $sub(2); return _limit; }
-  void set limit(int limit) { if (limit != null && limit == _limit) return; _limit = limit ?? 10; $pub(2); }
-
-  String get startKey { $sub(3); return _startKey; }
-  void set startKey(String startKey) { if (startKey != null && startKey == _startKey) return; _startKey = startKey ?? ""; $pub(3); }
-
-  String get parentKey { $sub(4); return _parentKey; }
-  void set parentKey(String parentKey) { if (parentKey != null && parentKey == _parentKey) return; _parentKey = parentKey ?? ""; $pub(4); }
-}
-
 
 
 
@@ -338,17 +260,6 @@ class ACResult {
       .._value = value
       .._id = id;
   }
-  static ACResult $createObservable({
-    String name = "",
-    String value = "",
-    int id = 0,
-  }) {
-    return new _ACResult()
-      .._name = name
-      .._value = value
-      .._id = id;
-  }
-
   static const int FN_NAME = 1;
   static const int FN_VALUE = 2;
   static const int FN_ID = 3;
@@ -369,17 +280,6 @@ class ACResult {
   int get id => _id;
   void set id(int id) { _id = id; }
 }
-class _ACResult extends ACResult with PubSub {
-  String get name { $sub(1); return _name; }
-  void set name(String name) { if (name != null && name == _name) return; _name = name ?? ""; $pub(1); }
-
-  String get value { $sub(2); return _value; }
-  void set value(String value) { if (value != null && value == _value) return; _value = value ?? ""; $pub(2); }
-
-  int get id { $sub(3); return _id; }
-  void set id(int id) { if (id != null && id == _id) return; _id = id ?? 0; $pub(3); }
-}
-
 /// message ACResult_PList {
 ///   repeated ACResult p = 1;
 /// }
@@ -460,19 +360,6 @@ class P1 {
       .._pgstart = pgstart
       .._prk = prk;
   }
-  static P1 $createObservable({
-    int value = 0,
-    int end = -1,
-    int pgstart = -1,
-    ParamRangeKey prk,
-  }) {
-    return new _P1()
-      .._value = value
-      .._end = end
-      .._pgstart = pgstart
-      .._prk = prk ?? ParamRangeKey.$createObservable();
-  }
-
   static const int FN_VALUE = 1;
   static const int FN_END = 2;
   static const int FN_PGSTART = 3;
@@ -499,20 +386,6 @@ class P1 {
   ParamRangeKey get prk => _prk;
   void set prk(ParamRangeKey prk) { _prk = prk; }
 }
-class _P1 extends P1 with PubSub {
-  int get value { $sub(1); return _value; }
-  void set value(int value) { if (value != null && value == _value) return; _value = value ?? 0; $pub(1); }
-
-  int get end { $sub(2); return _end; }
-  void set end(int end) { if (end != null && end == _end) return; _end = end ?? -1; $pub(2); }
-
-  int get pgstart { $sub(3); return _pgstart; }
-  void set pgstart(int pgstart) { if (pgstart != null && pgstart == _pgstart) return; _pgstart = pgstart ?? -1; $pub(3); }
-
-  ParamRangeKey get prk { $sub(4); return _prk; }
-  void set prk(ParamRangeKey prk) { if (prk != null && prk == _prk) return; _prk = prk; $pub(4); }
-}
-
 
 
 
@@ -558,19 +431,6 @@ class P4 {
       .._pgstart = pgstart
       .._prk = prk;
   }
-  static P4 $createObservable({
-    int value = 0,
-    int end = -1,
-    int pgstart = -1,
-    ParamRangeKey prk,
-  }) {
-    return new _P4()
-      .._value = value
-      .._end = end
-      .._pgstart = pgstart
-      .._prk = prk ?? ParamRangeKey.$createObservable();
-  }
-
   static const int FN_VALUE = 1;
   static const int FN_END = 2;
   static const int FN_PGSTART = 3;
@@ -597,20 +457,6 @@ class P4 {
   ParamRangeKey get prk => _prk;
   void set prk(ParamRangeKey prk) { _prk = prk; }
 }
-class _P4 extends P4 with PubSub {
-  int get value { $sub(1); return _value; }
-  void set value(int value) { if (value != null && value == _value) return; _value = value ?? 0; $pub(1); }
-
-  int get end { $sub(2); return _end; }
-  void set end(int end) { if (end != null && end == _end) return; _end = end ?? -1; $pub(2); }
-
-  int get pgstart { $sub(3); return _pgstart; }
-  void set pgstart(int pgstart) { if (pgstart != null && pgstart == _pgstart) return; _pgstart = pgstart ?? -1; $pub(3); }
-
-  ParamRangeKey get prk { $sub(4); return _prk; }
-  void set prk(ParamRangeKey prk) { if (prk != null && prk == _prk) return; _prk = prk; $pub(4); }
-}
-
 
 
 
@@ -656,19 +502,6 @@ class P8 {
       .._pgstart = pgstart
       .._prk = prk;
   }
-  static P8 $createObservable({
-    int value = 0,
-    int end = -1,
-    int pgstart = -1,
-    ParamRangeKey prk,
-  }) {
-    return new _P8()
-      .._value = value
-      .._end = end
-      .._pgstart = pgstart
-      .._prk = prk ?? ParamRangeKey.$createObservable();
-  }
-
   static const int FN_VALUE = 1;
   static const int FN_END = 2;
   static const int FN_PGSTART = 3;
@@ -695,20 +528,6 @@ class P8 {
   ParamRangeKey get prk => _prk;
   void set prk(ParamRangeKey prk) { _prk = prk; }
 }
-class _P8 extends P8 with PubSub {
-  int get value { $sub(1); return _value; }
-  void set value(int value) { if (value != null && value == _value) return; _value = value ?? 0; $pub(1); }
-
-  int get end { $sub(2); return _end; }
-  void set end(int end) { if (end != null && end == _end) return; _end = end ?? -1; $pub(2); }
-
-  int get pgstart { $sub(3); return _pgstart; }
-  void set pgstart(int pgstart) { if (pgstart != null && pgstart == _pgstart) return; _pgstart = pgstart ?? -1; $pub(3); }
-
-  ParamRangeKey get prk { $sub(4); return _prk; }
-  void set prk(ParamRangeKey prk) { if (prk != null && prk == _prk) return; _prk = prk; $pub(4); }
-}
-
 
 
 
@@ -754,19 +573,6 @@ class PD {
       .._pgstart = pgstart
       .._prk = prk;
   }
-  static PD $createObservable({
-    int value = 0,
-    int end = 0,
-    int pgstart = 0,
-    ParamRangeKey prk,
-  }) {
-    return new _PD()
-      .._value = value
-      .._end = end
-      .._pgstart = pgstart
-      .._prk = prk ?? ParamRangeKey.$createObservable();
-  }
-
   static const int FN_VALUE = 1;
   static const int FN_END = 2;
   static const int FN_PGSTART = 3;
@@ -793,20 +599,6 @@ class PD {
   ParamRangeKey get prk => _prk;
   void set prk(ParamRangeKey prk) { _prk = prk; }
 }
-class _PD extends PD with PubSub {
-  int get value { $sub(1); return _value; }
-  void set value(int value) { if (value != null && value == _value) return; _value = value ?? 0; $pub(1); }
-
-  int get end { $sub(2); return _end; }
-  void set end(int end) { if (end != null && end == _end) return; _end = end ?? 0; $pub(2); }
-
-  int get pgstart { $sub(3); return _pgstart; }
-  void set pgstart(int pgstart) { if (pgstart != null && pgstart == _pgstart) return; _pgstart = pgstart ?? 0; $pub(3); }
-
-  ParamRangeKey get prk { $sub(4); return _prk; }
-  void set prk(ParamRangeKey prk) { if (prk != null && prk == _prk) return; _prk = prk; $pub(4); }
-}
-
 
 
 
@@ -852,19 +644,6 @@ class PB {
       .._pgstart = pgstart
       .._prk = prk;
   }
-  static PB $createObservable({
-    String value = "",
-    String end = "",
-    String pgstart = "",
-    ParamRangeKey prk,
-  }) {
-    return new _PB()
-      .._value = value
-      .._end = end
-      .._pgstart = pgstart
-      .._prk = prk ?? ParamRangeKey.$createObservable();
-  }
-
   static const int FN_VALUE = 1;
   static const int FN_END = 2;
   static const int FN_PGSTART = 3;
@@ -891,20 +670,6 @@ class PB {
   ParamRangeKey get prk => _prk;
   void set prk(ParamRangeKey prk) { _prk = prk; }
 }
-class _PB extends PB with PubSub {
-  String get value { $sub(1); return _value; }
-  void set value(String value) { if (value != null && value == _value) return; _value = value ?? ""; $pub(1); }
-
-  String get end { $sub(2); return _end; }
-  void set end(String end) { if (end != null && end == _end) return; _end = end ?? ""; $pub(2); }
-
-  String get pgstart { $sub(3); return _pgstart; }
-  void set pgstart(String pgstart) { if (pgstart != null && pgstart == _pgstart) return; _pgstart = pgstart ?? ""; $pub(3); }
-
-  ParamRangeKey get prk { $sub(4); return _prk; }
-  void set prk(ParamRangeKey prk) { if (prk != null && prk == _prk) return; _prk = prk; $pub(4); }
-}
-
 
 
 
@@ -950,19 +715,6 @@ class PK {
       .._pgstart = pgstart
       .._prk = prk;
   }
-  static PK $createObservable({
-    String value = "",
-    String end = "",
-    String pgstart = "",
-    ParamRangeKey prk,
-  }) {
-    return new _PK()
-      .._value = value
-      .._end = end
-      .._pgstart = pgstart
-      .._prk = prk ?? ParamRangeKey.$createObservable();
-  }
-
   static const int FN_VALUE = 1;
   static const int FN_END = 2;
   static const int FN_PGSTART = 3;
@@ -989,20 +741,6 @@ class PK {
   ParamRangeKey get prk => _prk;
   void set prk(ParamRangeKey prk) { _prk = prk; }
 }
-class _PK extends PK with PubSub {
-  String get value { $sub(1); return _value; }
-  void set value(String value) { if (value != null && value == _value) return; _value = value ?? ""; $pub(1); }
-
-  String get end { $sub(2); return _end; }
-  void set end(String end) { if (end != null && end == _end) return; _end = end ?? ""; $pub(2); }
-
-  String get pgstart { $sub(3); return _pgstart; }
-  void set pgstart(String pgstart) { if (pgstart != null && pgstart == _pgstart) return; _pgstart = pgstart ?? ""; $pub(3); }
-
-  ParamRangeKey get prk { $sub(4); return _prk; }
-  void set prk(ParamRangeKey prk) { if (prk != null && prk == _prk) return; _prk = prk; $pub(4); }
-}
-
 
 
 
@@ -1048,19 +786,6 @@ class PS {
       .._pgstart = pgstart
       .._prk = prk;
   }
-  static PS $createObservable({
-    String value = "",
-    String end = "",
-    String pgstart = "",
-    ParamRangeKey prk,
-  }) {
-    return new _PS()
-      .._value = value
-      .._end = end
-      .._pgstart = pgstart
-      .._prk = prk ?? ParamRangeKey.$createObservable();
-  }
-
   static const int FN_VALUE = 1;
   static const int FN_END = 2;
   static const int FN_PGSTART = 3;
@@ -1087,20 +812,6 @@ class PS {
   ParamRangeKey get prk => _prk;
   void set prk(ParamRangeKey prk) { _prk = prk; }
 }
-class _PS extends PS with PubSub {
-  String get value { $sub(1); return _value; }
-  void set value(String value) { if (value != null && value == _value) return; _value = value ?? ""; $pub(1); }
-
-  String get end { $sub(2); return _end; }
-  void set end(String end) { if (end != null && end == _end) return; _end = end ?? ""; $pub(2); }
-
-  String get pgstart { $sub(3); return _pgstart; }
-  void set pgstart(String pgstart) { if (pgstart != null && pgstart == _pgstart) return; _pgstart = pgstart ?? ""; $pub(3); }
-
-  ParamRangeKey get prk { $sub(4); return _prk; }
-  void set prk(ParamRangeKey prk) { if (prk != null && prk == _prk) return; _prk = prk; $pub(4); }
-}
-
 
 
 
